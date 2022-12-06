4788199b958fea3154873565a42011110d095a95
Kavitha <kavithamd.kavi@gmail.com>
2022-12-07 00:30:21 +0530

    fix : made some code changes

--- index.js
+++ index.js
@@ -5,13 +5,24 @@ console.log("Welcome " + userName + " to DO YOU KNOW Kavitha?");
 
 console.log("------------------------");
 
+var highScore = [
+  {
+    name: "kmd",
+    score: 5,
+  },
+  {
+    name: "savitha",
+    score: 4,
+  },
+];
+
 function play(question, answer) {
   var userAnswer = readlineSync.question(question);
-  if (userAnswer === answer) {
-    console.log("right👌");
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+    console.log("right!");
     score++;
   } else {
-    console.log("wrong👎");
+    console.log("wrong!");
   }
 
   console.log("current score: ", score);
@@ -50,5 +61,9 @@ for (var i = 0; i < questions.length; i++) {
 }
 
 console.log("Your Total Score is : ", score);
+console.log(
+  "Check out the high scores, if you should be there ping me and I'll update it,"
+);
+highScore.map((score) => console.log(score.name, ":", score.score));
 
-console.log("Thank you for playing");
+console.log("Thank you for playing !");
